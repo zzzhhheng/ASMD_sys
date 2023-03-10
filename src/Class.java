e5ec859da7ceead1e444c6ac40e8d5d0afc0e221
HuwaCoffee <116842137+HuwaCoffee@users.noreply.github.com>
2023-03-10 22:58:17 +0800

    Create Class.java

---
+++ src/Class.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+/*班级类中包含某个班级的所有学生信息
+可以通过声明一个班级类型的变量进行学校信息的存储和增删改查*/
+public class Class {
+    private ArrayList<Student>studentList;   //储存学生信息的数组
+    private int ClassId;                     //班级id
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public int getClassId() {
+        return ClassId;
+    }
+
+    public void setClassId(int classId) {
+        ClassId = classId;
+    }
+}
