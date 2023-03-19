8ac0bb055e7906d6ef8b95e2e60da5f91b1a0273
liguohhh <2796719916@qq.com>
2023-03-19 22:25:40 +0800

    Update Exercise.java

--- src/exercise/Exercise.java
+++ src/exercise/Exercise.java
@@ -13,18 +13,6 @@ public class Exercise {
     private ArrayList<BinaryOperation> operationList = new ArrayList<>();
     private int current = 0;//用于遍历数据集
 
-    public ArrayList<BinaryOperation> generateBinaryExercise(int operationCount) {
-        BinaryOperation anOperation = null;
-        while (operationCount > 0) {
-            do {
-                anOperation = generateOperation();
-
-            } while (operationList.contains(anOperation));
-            operationCount--;
-            operationList.add(anOperation);
-        }
-        return operationList;
-    }
 
     public ArrayList<BinaryOperation> generateAdditionExercise(int operationCount) {
         BinaryOperation anOperation = null;
@@ -83,23 +71,6 @@ public class Exercise {
         }
         return operationList;
     }
-//随机生成加减乘除中的任意一种
-    private BinaryOperation generateOperation() {
-        Random random = new Random();
-        BinaryOperation operation=null;
-        int opValue = random.nextInt(4);
-        if (opValue == 1) {
-            operation= new AdditionOperation();
-        }else if(opValue==2) {
-            operation=new SubstractOperation();
-        }else if(opValue==3){
-            operation= new MultiplyOperation();
-        }else {
-            operation=new DivisionOperation();
-        }return  operation;
-    }
-
-
 
 
     //下面两个方法用于实现遍历数据
