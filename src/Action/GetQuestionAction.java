c04aae94a12c5dbd1a93b76d880e25643aeb1258
HuwaCoffee <116842137+HuwaCoffee@users.noreply.github.com>
2023-03-21 20:52:55 +0800

    Update GetQuestionAction.java

--- src/Action/GetQuestionAction.java
+++ src/Action/GetQuestionAction.java
@@ -1,5 +1,6 @@
 package Action;
 
+import com.opensymphony.xwork2.ActionContext;
 import exercise.BinaryOperation;
 import exercise.MysqlTool;
 import java.sql.Connection;
@@ -7,11 +8,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Random;
+import com.opensymphony.xwork2.Action;
+
+
 
 //当学生点击练习或查看试卷时，前端调用此类，获取题目数组，并显示给用户
-public class GetQuestionAction {
+public class GetQuestionAction implements Action {
     private ArrayList<BinaryOperation> questions;//题目数组
-
     //数据库相关属性
     private MysqlTool mysql=new MysqlTool();
     private Connection con;
@@ -21,10 +24,12 @@ public class GetQuestionAction {
         return questions;
     }
 
-    //获取普通练习的题目，默认10道题。有重载方法，可以在括号内填想要获取的题目数
-    public void getNormalExercise(){
+
+    //获取普通练习的题目，默认10道题。可以修改方法让用户输入题目数
+    @Override
+    public String execute() throws Exception {
         con=mysql.openDB();
-        //从数据库中随机抽取题目
+        //从数据库中随机抽10道题，存入questions
         try {
             for (int i = 0; i < 10; i++) {
                 statement=con.createStatement();
@@ -32,41 +37,10 @@ public class GetQuestionAction {
                 //随机出题，如果想要定向出题（如只有加，只有减，或者只有乘除），可以限定随机数的范围
                 int questionId= random.nextInt(100001);
                 //通过题目id查询题目的所有信息（id,factor1,operator,factor2,result），并存入questions数组
-                
+
                 /*待完成
                 待完成
-                待完成 
                 待完成
-                待完成
-                待完成
-                待完成
-                待完成
-                待完成*/
-
-
-            }
-                statement.close();
-                con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-    }
-
-    //普通练习的重载方法，可以指定获取题目的数量
-    public void getNormalExercise(int number){
-        con=mysql.openDB();
-        //从数据库中随机抽取题目
-        try {
-            for (int i = 0; i < number; i++) {
-                statement=con.createStatement();
-                Random random = new Random();
-                //随机出题，如果想要定向出题（如只有加，只有减，或者只有乘除），可以限定随机数的范围
-                int questionId= random.nextInt(100001);
-                //通过题目id查询题目的所有信息（id,factor1,operator,factor2,result），并存入questions数组
-                
-                /*待完成
-                待完成
-                待完成 
                 待完成
                 待完成
                 待完成
@@ -81,5 +55,17 @@ public class GetQuestionAction {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
+
+
+        ActionContext ac=ActionContext.getContext();
+        //把习题数组保存到application中
+        ac.getApplication().put("questions",getQuestions());
+        //返回SUCCESS，题目返回成功
+        return SUCCESS;
     }
+
+
+
 }
